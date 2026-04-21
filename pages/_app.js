6a110a29c92373966e6ae4dd7e374af6a51af61c
pacakim <super.kimpaca@gmail.com>
2026-04-21 17:48:39 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style>{`
+        @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500&family=Noto+Sans+KR:wght@300;400;500&display=swap');
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
